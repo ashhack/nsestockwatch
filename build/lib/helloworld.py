b31b58dd1955e37a5f6d21ecab34212018974bf6
Ashutosh V Singh <ashutosh.v.singh@zs.com>
2021-02-23 23:09:31 +0530

    First Commit

---
+++ build/lib/helloworld.py
@@ -0,0 +1,2 @@
+def print_hw():
+    print("hello world!")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="helloworld",
+    version="0.0.1",
+    description='say hello',
+    py_modules=["helloworld"],
+    package_dir={'':'src'}
+)
